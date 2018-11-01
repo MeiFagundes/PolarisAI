7438de795c277463c63f32fb4626c838f80a02e2
Mei Fagundes <MeiFagundes@outlook.com>
2018-10-31 22:30:29 -0300

    Implement Search Skill that opens a Google search in the Default Browser

--- Skills/Search.cs
+++ Skills/Search.cs
@@ -3,9 +3,23 @@
 namespace POLARIS.Skills {
 	public static class Search {
 		
+        /// <summary>
+        /// Searches something on Google in the Default browser.
+        /// </summary>
+        /// <param name="dialog"></param>
 		public static void Execute(Dialog dialog){
-			Console.WriteLine("Inside Skills.Search.Execute");
-			// To-do
-		}
+
+            if (dialog.Phrase[dialog.VerbsIndex[0] + 1] == "about")
+                dialog.Phrase.Remove("about");
+
+            if (dialog.Phrase[dialog.Phrase.Count - 1] == "please")
+                dialog.Phrase.Remove("please");
+
+            // Formatting search query to insert it into google search parameters
+            String search = String.Join("+",
+                dialog.Phrase.GetRange(dialog.VerbsIndex[0] + 1, dialog.Phrase.Count - 1));
+
+            System.Diagnostics.Process.Start("http://google.com/search?q=" + search);
+        }
 	}
 }

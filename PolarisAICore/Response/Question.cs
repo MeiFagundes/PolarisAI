1ec4096a5d4a62d2285cf665813898a057d807e9
Mei Fagundes <MeiFagundes@outlook.com>
2019-11-10 12:44:05 -0300

    Set static Response handling as obsolete

---
+++ PolarisAICore/Response/Question.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PolarisAICore.Response {
+
+    [Obsolete("Static cognition is obsolete, please use NLP cognition instead")]
+    public static class Question {
+
+        public static void SetResponse(Utterance d) {
+
+            if (d.Contains("can") && d.Contains("you") && d.GetPositionDifference("can", "you") == -1) {
+
+                d.Response = "Well, maybe I can. I don't know it yet...";
+                return;
+            }
+
+            d.Response = "Sorry, I don't know how to interpret generic questions yet.";
+        }
+    }
+}

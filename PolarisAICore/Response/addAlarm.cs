b601f3d301e5197175bcfccb60eb32a35772fd16
Mei Fagundes <MeiFagundes@outlook.com>
2019-11-10 12:44:54 -0300

    Add addAlarm Response handling

---
+++ PolarisAICore/Response/addAlarm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolarisAICore.Response {
+    class AddAlarm {
+
+        static readonly Random _random = new Random();
+
+        static readonly String[] _responses =
+        {
+            "Of course, I've set an alarm for",
+            "Sure, I've set an alarm for",
+            "Got it! I've set an alarm for",
+            "You don't have to ask again! I've set an alarm for",
+        };
+
+        static readonly String[] _noEntityResponses =
+        {
+            "Of course, what time do you want your alarm to be set to?",
+            "Sure, do you want me to set your alarm for what time?",
+            "Alright, what time do you want your alarm to be set to?",
+            "You don't have to ask again! Do you want me to set your alarm to what time?"
+        };
+
+        public static String SetResponse(Utterance u) {
+
+            if (u.Entity["time"] != null)
+                if (u.Entity["entity"] != null)
+                    return $"{_responses[_random.Next(_responses.Length)]} {u.Entity["entity"]}, {u.Entity["time"]}.";
+                else
+                    return $"{_responses[_random.Next(_responses.Length)]} {u.Entity["time"]}.";
+            else
+                return _noEntityResponses[_random.Next(_noEntityResponses.Length)];
+        }
+    }
+}
